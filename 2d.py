1f1d72dc76cf884836124036d711232dbdd89e71
legobadman <lance5158387@gmail.com>
2021-03-03 10:18:08 +0800

    添加newton法和停止准则

--- 2d.py
+++ 2d.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from Arrow3D import Arrow3D
 
-
+#User specific:
 def f_(x,y):
     return 1./3 * x**2 + 1./2 * y**2
 
@@ -12,12 +12,30 @@ def f(x):
 def grad(x):
     return np.array([2./3 * x[0], x[1]])
 
+def hessian(x):
+    return np.array([[2./3, 0], [0, 1]])
+#End
+
+
 def get_search_direction(x, f, grad, type='gradient'):
     second_order = 1
+    lamba_square = 0
     if type == 'newton':
-        pass
-    delta_x = -1 * second_order * grad(x)
-    return delta_x
+        grad_x = grad(x)
+        H_x = hessian(x)
+        inv_Hx = np.linalg.inv(H_x)
+        second_order = -1 * inv_Hx
+        delta_x = second_order.dot(grad_x)
+        lamba_square = np.sqrt(grad_x.dot(inv_Hx).dot(grad_x)) 
+    else:
+        delta_x = -1 * grad(x)
+    return delta_x, lamba_square
+
+def stop_criteria(delta_x, lambda_square, method, epsilon):
+    if method == 'newton':
+        return lambda_square / 2.0 <= epsilon
+    else:
+        return np.linalg.norm(delta_x) <= epsilon
 
 def get_step(f, x, grad_x, delta_x, type='backtracing'):
     if type == 'backtracing':
@@ -60,14 +78,15 @@ def annote_descent(x, t, delta_x, f):
                      arrowprops=dict(arrowstyle="->",color = 'black',
                                      connectionstyle='arc3'))
 
-epslion = 0.05
+epsilon = 0.05
 x_start = np.array([-2,-2]) #这个x是二元坐标(x,y)
 x = x_start
+method = 'newton'
 
 while True:
     grad_x = grad(x)
-    delta_x = get_search_direction(x, f, grad)
-    if np.linalg.norm(delta_x) <= epslion:
+    delta_x, lambda_square = get_search_direction(x, f, grad, method)
+    if stop_criteria(delta_x, lambda_square, method, epsilon):
         x_star = x
         break
     t = get_step(f, x, grad_x, delta_x, type='backtracing')
